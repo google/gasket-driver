860ed615fa42ba3ab4984fca75035b72e989c226
Leonid Lobachev <leonidl@google.com>
2020-08-07 15:00:59 -0700

    Change ioremap_nocache to ioremap.
    ioremap defaults to ioremap_nocache since 2.6 and latter is removed in
    5.6 kernel.
    See https://github.com/torvalds/linux/commit/4bdc0d676a643140bdf17dbf7eafedee3d496a3c
    
    Change-Id: Ifd9ff9db5937abf487d57861465cd58b4df18ac3

--- gasket_core.c
+++ gasket_core.c
@@ -316,8 +316,8 @@ static int gasket_map_pci_bar(struct gasket_dev *gasket_dev, int bar_num)
 	}
 
 	gasket_dev->bar_data[bar_num].virt_base =
-		ioremap_nocache(gasket_dev->bar_data[bar_num].phys_base,
-				gasket_dev->bar_data[bar_num].length_bytes);
+		ioremap(gasket_dev->bar_data[bar_num].phys_base,
+			gasket_dev->bar_data[bar_num].length_bytes);
 	if (!gasket_dev->bar_data[bar_num].virt_base) {
 		dev_err(gasket_dev->dev,
 			"Cannot remap BAR %d memory region %p\n",
